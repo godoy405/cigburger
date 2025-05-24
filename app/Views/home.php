1a0b346e4e394aa8c63a57ef97699552e4691b43
Gilberto <gilbertonerigodoy@gmail.com>
2025-05-24 20:29:43 -0300

    Criando o Projeto CigBurber - Parte 2 de 7

---
+++ app/Views/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CIGBurger</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="<?= base_url('assets/images/logo.png') ?>" type="image/png">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/bootstrap.min.css') ?>">
+    <!-- app.css -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/app.css') ?>">
+</head>
+<body>
+
+    <!-- nav -->
+    <nav class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col p-3">
+                <!-- logo -->
+                <img src="<?= base_url('assets/images/logo.png') ?>" alt="CIGBurger Logo">
+            </div>
+            <div class="col p-3 pe-5 d-flex flex-row justify-content-end">
+                <div><a class="nav-link ms-5" href="<?= site_url('/') ?>">Início</a></div>
+                <div><a class="nav-link ms-5" href="<?= site_url('products') ?>">Produtos</a></div>
+                <div><a class="nav-link ms-5" href="<?= site_url('where_we_are') ?>">Onde estamos?</a></div>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+    <h1>Home</h1>
+
+    <!-- bootstrap -->
+    <script src="<?= base_url('assets/bootstrap/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
---
+++ app/Views/where_we_are.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CIGBurger</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/bootstrap.min.css') ?>">
+</head>
+<body>
+    <h1>Onde estamos</h1>
+    <!-- bootstrap -->
+    <script src="<?= base_url('assets/bootstrap/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
